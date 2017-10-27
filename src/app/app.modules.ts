510ecf6301c72fa1f49c954a36918ed58b87e741
Mauricio Marinho <mauriciocmarinho@gmail.com>
2017-10-26 23:31:53 -0400

    +testes

--- src/app/app.modules.ts
+++ src/app/app.modules.ts
@@ -4,36 +4,20 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+
 import { HeroesComponent } from './hero/heroes.component';
 import { HeroDetailComponent } from './hero/hero-detail.component';
-
 import { DashboardComponent } from './dashboard/dashboard.component';
 
 import { HeroService } from './services/hero.service';
+
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
     imports: [
         BrowserModule,
         FormsModule,
-        RouterModule.forRoot([
-            {
-                path: '',
-                redirectTo: '/dashboard',
-                pathMatch: 'full'
-            },
-            {
-                path: 'heroes',
-                component: HeroesComponent
-            },
-            {
-                path: 'dashboard',
-                component: DashboardComponent
-            },
-            {
-                path: 'detail/:id',
-                component: HeroDetailComponent
-            }
-        ])
+        AppRoutingModule
     ],
     declarations: [
         AppComponent,
